6253da81a11b16d4052bf397aeb81fe58fdb5b29
saraanv <saraalinezhad1998@gmail.com>
2024-05-01 19:36:53 +0330

    working with colors and customizing colors and pesonolizing colors in tailwind config

--- mytailwindproject/tailwind.config.js
+++ mytailwindproject/tailwind.config.js
@@ -1,8 +1,19 @@
+// const colors = require('tailwindcss/colors')
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        my : {
+          50 : "rgba (255,0,0,0.5)" ,
+          100 : "rgba (255,0,0,1)" 
+        } 
+      }
+    },
   },
   plugins: [],
 }
